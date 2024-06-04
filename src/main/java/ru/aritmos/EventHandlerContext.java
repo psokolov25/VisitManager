78665514c4ca9171cf3fd8659bacdde3af33bc1a
psokolov <pavel.sokolov@q-systems.ru>
2024-06-04 05:23:55 -0700

    Added event handler

--- src/main/java/ru/aritmos/EventHandlerContext.java
+++ src/main/java/ru/aritmos/EventHandlerContext.java
@@ -18,10 +18,18 @@ public class EventHandlerContext {
             log.info("Event {} of Business error handled!",event);
         }
     }
+    static class VisitCallHandler implements EventHandler {
+
+        @Override
+        public void Handle(Event event) {
+            log.info("Event {} of call of visit handled!",event);
+        }
+    }
 
     @PostConstruct
     void AddHandlers() {
         BusinesErrorHandler businesErrorHandler = new BusinesErrorHandler();
         KaffkaListener.addAllEventHandler("BUSINESS_ERROR", businesErrorHandler);
+        KaffkaListener.addServiceEventHandler("VISIT_CALLED", businesErrorHandler);
     }
 }
