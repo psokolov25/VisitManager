b9c00a63006bb7896561b1c1f6e70866fe355117
psokolov <pavel.sokolov@q-systems.ru>
2024-06-05 03:41:21 -0700

    Реализовал выполнение обработки события в отдельном потоке

--- src/main/java/ru/aritmos/events/model/EventHandler.java
+++ src/main/java/ru/aritmos/events/model/EventHandler.java
@@ -1,5 +1,9 @@
 package ru.aritmos.events.model;
 
+import io.micronaut.scheduling.TaskExecutors;
+import io.micronaut.scheduling.annotation.ExecuteOn;
+
 public interface EventHandler {
+@ExecuteOn(TaskExecutors.IO)
     void Handle(Event event);
 }
