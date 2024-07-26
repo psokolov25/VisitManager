b14ed2369fc0bf248d77d4a21d56469b2e8e780f
Pavel Sokolov <psokolov@gmail.com>
2024-07-26 16:25:27 +0300

    Убрал конфликты кода между ветками

--- src/main/java/ru/aritmos/model/visit/VisitEvent.java
+++ src/main/java/ru/aritmos/model/visit/VisitEvent.java
@@ -35,7 +35,7 @@ public enum VisitEvent {
         {
 
             add(BACK_TO_QUEUE);
-            add(PLACED_IN_QUEUE);
+
 
         }
     };
@@ -46,13 +46,12 @@ public enum VisitEvent {
 
 
     private static final Map<VisitEvent, TransactionCompletionStatus> transactionStatus = Map.ofEntries(
-            Map.entry(PLACED_IN_QUEUE, TransactionCompletionStatus.BACK_TO_QUEUE)
+            Map.entry(BACK_TO_QUEUE, TransactionCompletionStatus.BACK_TO_QUEUE)
 
     );
 
     public static TransactionCompletionStatus getStatus(VisitEvent visitEvent) {
-        if (transactionStatus.containsKey(visitEvent))
-        {
+        if (transactionStatus.containsKey(visitEvent)) {
             return transactionStatus.get(visitEvent);
         }
         return null;
@@ -64,13 +63,16 @@ public enum VisitEvent {
             Map.entry(PLACED_IN_QUEUE, List.of(CALLED)),
             Map.entry(CALLED, List.of(RECALLED, START_SERVING, NO_SHOW, BACK_TO_QUEUE)),
             Map.entry(RECALLED, List.of(RECALLED, START_SERVING, NO_SHOW, BACK_TO_QUEUE)),
-            Map.entry(START_SERVING, List.of(BACK_TO_QUEUE, STOP_SERVING, END, TRANSFER_TO_SERVICE_POINT_POOL, TRANSFER_TO_USER_POOL, TRANSFER_TO_QUEUE, ADD_SERVICE)),
-            Map.entry(STOP_SERVING, List.of(PLACED_IN_QUEUE, END, VISIT_END_TRANSACTION)),
+            Map.entry(START_SERVING, List.of(BACK_TO_QUEUE, STOP_SERVING, END, TRANSFER_TO_SERVICE_POINT_POOL, TRANSFER_TO_USER_POOL, TRANSFER_TO_QUEUE, ADD_SERVICE, ADDED_DELIVERED_SERVICE, ADDED_SERVICE_RESULT)),
+            Map.entry(STOP_SERVING, List.of(PLACED_IN_QUEUE, END, VISIT_END_TRANSACTION, BACK_TO_QUEUE)),
             Map.entry(NO_SHOW, List.of()),
             Map.entry(END, List.of()),
             Map.entry(TRANSFER_TO_USER_POOL, List.of(CALLED)),
             Map.entry(VISIT_END_TRANSACTION, List.of(PLACED_IN_QUEUE)),
             Map.entry(VISIT_TRANSFER_FROM_QUEUE, List.of(CALLED)),
+            Map.entry(ADDED_DELIVERED_SERVICE, List.of(ADDED_DELIVERED_SERVICE, STOP_SERVING, ADDED_SERVICE_RESULT, ADDED_DELIVERED_SERVICE_RESULT)),
+            Map.entry(ADDED_SERVICE_RESULT, List.of(ADDED_DELIVERED_SERVICE, STOP_SERVING, ADDED_SERVICE_RESULT, ADDED_DELIVERED_SERVICE)),
+            Map.entry(ADDED_DELIVERED_SERVICE_RESULT, List.of(ADDED_DELIVERED_SERVICE, STOP_SERVING, ADDED_SERVICE_RESULT, ADDED_DELIVERED_SERVICE)),
             // TRANSFER_TO_SERVICE_POOL, List.of(CALLED),
             // TRANSFER_TO_SERVICE_POINT_POOL, List.of(CALLED),
             // BACK_TO_QUEUE, List.of(CALLED),
@@ -78,28 +80,34 @@ public enum VisitEvent {
             Map.entry(ADD_SERVICE, List.of(PLACED_IN_QUEUE, BACK_TO_QUEUE, STOP_SERVING, END, TRANSFER_TO_SERVICE_POINT_POOL, TRANSFER_TO_USER_POOL, TRANSFER_TO_QUEUE, ADD_SERVICE))
     );
 
-    private static final Map<VisitEvent, VisitState> visitStateMap = Map.of(
-            CREATED, VisitState.CREATED,
-            PLACED_IN_QUEUE, VisitState.WAITING_IN_QUEUE,
-            CALLED, VisitState.CALLED,
-            RECALLED, VisitState.CALLED,
-            START_SERVING, VisitState.SERVING,
-            STOP_SERVING, VisitState.CREATED,
-            NO_SHOW, VisitState.END,
-            END, VisitState.END,
-            BACK_TO_QUEUE, VisitState.WAITING_IN_QUEUE);
-//TRANSFER_TO_SERVICE_POINT_POOL, List.of(VisitState.WAITING_IN_SP_POOL),
-//BACK_TO_QUEUE, List.of(VisitState.WAITING_IN_QUEUE)
-// TRANSFER_TO_QUEUE, List.of(VisitState.WAITING_IN_QUEUE),
-//  ADD_SERVICE, List.of()
-//);
+    private static final Map<VisitEvent, VisitState> visitStateMap = Map.ofEntries(
+            Map.entry(CREATED, VisitState.CREATED),
+            Map.entry(PLACED_IN_QUEUE, VisitState.WAITING_IN_QUEUE),
+            Map.entry(CALLED, VisitState.CALLED),
+            Map.entry(RECALLED, VisitState.CALLED),
+            Map.entry(START_SERVING, VisitState.SERVING),
+            Map.entry(ADDED_DELIVERED_SERVICE, VisitState.SERVING),
+            Map.entry(ADDED_SERVICE_RESULT, VisitState.SERVING),
+            Map.entry(STOP_SERVING, VisitState.CREATED),
+            Map.entry(NO_SHOW, VisitState.END),
+            Map.entry(END, VisitState.END),
+            Map.entry(BACK_TO_QUEUE, VisitState.WAITING_IN_QUEUE),
+            Map.entry(ADDED_DELIVERED_SERVICE_RESULT, VisitState.SERVING));
+
+    //TRANSFER_TO_SERVICE_POINT_POOL, List.of(VisitState.WAITING_IN_SP_POOL),
+    //BACK_TO_QUEUE, List.of(VisitState.WAITING_IN_QUEUE)
+    // TRANSFER_TO_QUEUE, List.of(VisitState.WAITING_IN_QUEUE),
+    //  ADD_SERVICE, List.of()
+    //);
 
     public boolean canBeNext(VisitEvent next) {
         return nextEvents.get(this).stream().anyMatch(e -> e.equals(next));
     }
 
     public VisitState getState() {
-        return visitState = visitStateMap.get(this);
+        visitState = visitStateMap.get(this);
+        return visitState;
     }
 
 }
+
