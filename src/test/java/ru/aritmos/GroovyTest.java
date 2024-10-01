e5cc0d0c214f9a72e3c0fe9b36db78d676955c44
Pavel Sokolov <psokolov@gmail.com>
2024-10-01 19:04:46 +0300

    Реализовал пробнее решение запуска интерпретируемых скриптов на groovy для реализации правил вызова

---
+++ src/test/java/ru/aritmos/GroovyTest.java
@@ -0,0 +1,89 @@
+package ru.aritmos;
+
+import groovy.lang.Binding;
+import groovy.lang.GroovyShell;
+import groovy.lang.Script;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.time.ZonedDateTime;
+import java.util.*;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import ru.aritmos.model.visit.Visit;
+
+@Slf4j
+@MicronautTest
+public class GroovyTest {
+  @SuppressWarnings("unchecked")
+  @Test
+  public void testGroovy() throws IOException, URISyntaxException, InterruptedException {
+
+    ClassLoader classLoader = getClass().getClassLoader();
+
+    // Объект привязки скрипта со средой выполнения (для передачи значений переменных, свойств и т д
+    // )
+    // и для получения значений переменных и свойств после выполнения скрипта
+    Binding binding = new Binding();
+    // Объект выполнения скрипта Groovy
+    GroovyShell shell = new GroovyShell(binding);
+    // Текст скрипта на groovy, подгружается из файла (возможно так же и другое хранение скрипта)
+    String scriptCode =
+        Files.readString(
+            Paths.get(Objects.requireNonNull(classLoader.getResource("test.groovy")).toURI()));
+    // Обработка скрипта перед выполнением
+    Script script = shell.parse(scriptCode);
+    // Передача двух тестовых визитов
+    binding.setVariable(
+        "visits",
+        new ArrayList<Visit>() {
+          {
+            add(
+                Visit.builder()
+                    .id("test1")
+                    .createDateTime(ZonedDateTime.now())
+                    .queueId("123")
+                    .build());
+            Thread.sleep(0);
+            add(
+                Visit.builder()
+                    .id("test2")
+                    .createDateTime(ZonedDateTime.now())
+                    .servicePointId("123")
+                    .build());
+            Thread.sleep(10);
+            add(
+                Visit.builder()
+                    .id("test2")
+                    .createDateTime(ZonedDateTime.now())
+                    .poolUserId("20")
+                    .build());
+          }
+        });
+    // Передача дополнительных параметров, в данном случае идентификаторы очереди, юзерпула и пула
+    // точки обслуживания
+    // из которых нужно извлекать визиты
+    binding.setVariable(
+        "params",
+        new HashMap<String, Object>() {
+          {
+            put("queueId", "123");
+            put("userPoolId", "123");
+            put("servicePointId", "123");
+          }
+        });
+    // Запуск выполнения скрипта
+    script.run();
+    // Получение значений всех переменных скрипта
+    var result = binding.getVariables();
+    // Получение оптимального визита из двух, согласно алгоритму описанному в groovya crhbgnt
+    Optional<Visit> optimalVisit = (Optional<Visit>) result.get("result");
+
+    Assertions.assertTrue(optimalVisit.isPresent());
+    Assertions.assertEquals(optimalVisit.get().getQueueId(), "123");
+    log.info("Оптимальный визит:{}!", optimalVisit);
+  }
+}
