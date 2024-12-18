205c41e2f7892230abb8bed82f258908ad132794
Pavel Sokolov <psokolov@gmail.com>
2024-12-18 09:17:41 +0300

    Удалил мусорный файл

--- A.java
+++
@@ -1,8 +0,0 @@
-Map<String, Object> clientCredentials = new HashMap<>();
-    clientCredentials.put("secret", secret);
-    clientCredentials.put("provider", "secret");
-
-    Configuration configuration =
-            new Configuration(keycloakUrl, realm, clientId, clientCredentials, null);
-
-    AuthzClient authzClient = AuthzClient.create(configuration);
