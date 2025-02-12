d51bae81a50b87ba1f356dc8b9fb71abc2cda107
Pavel Sokolov <psokolov@gmail.com>
2025-02-12 13:53:39 +0300

    Поднял версию

--- src/main/java/ru/aritmos/Application.java
+++ src/main/java/ru/aritmos/Application.java
@@ -11,7 +11,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.annotation.PostConstruct;
 
 @OpenAPIDefinition(
-    info = @Info(title = "VisitManagement", version = "alpha_0.1.25"),
+    info = @Info(title = "VisitManagement", version = "alpha_02.25"),
     tags = {
       @Tag(name = "Зона обслуживания"),
       @Tag(name = "Зона ожидания"),
