10f1bdfed84a2faee5e206e4801b76274697ee7f
Pavel Sokolov <psokolov@gmail.com>
2025-03-13 11:21:55 +0300

    Добавил событие включение автовызова при вызове из пустой очереди

--- src/main/java/ru/aritmos/exceptions/BusinessException.java
+++ src/main/java/ru/aritmos/exceptions/BusinessException.java
@@ -10,6 +10,7 @@ import ru.aritmos.events.model.Event;
 import ru.aritmos.events.services.EventService;
 
 @Slf4j
+@SuppressWarnings("unused")
 public class BusinessException extends RuntimeException {
 
   final EventService eventService;
@@ -28,6 +29,39 @@ public class BusinessException extends RuntimeException {
             .body(businessError)
             .build());
     log.error(errorMessage);
+    throw new HttpStatusException(status, errorMessage);
+  }
+  public BusinessException(String errorMessage,String errorLogMessage, EventService eventService, HttpStatus status) {
+    super(errorMessage);
+    BusinessError businessError = new BusinessError();
+    businessError.setMessage(errorMessage);
+    this.eventService = eventService;
+    eventService.send(
+            "*",
+            false,
+            Event.builder()
+                    .eventDate(ZonedDateTime.now())
+                    .eventType("BUSINESS_ERROR")
+                    .body(businessError)
+                    .build());
+    log.error(errorLogMessage);
+    throw new HttpStatusException(status, errorMessage);
+  }
+
+  public BusinessException(String errorMessage,String errorLogMessage,String errorEventMessage, EventService eventService, HttpStatus status) {
+    super(errorMessage);
+    BusinessError businessError = new BusinessError();
+    businessError.setMessage(errorEventMessage);
+    this.eventService = eventService;
+    eventService.send(
+            "*",
+            false,
+            Event.builder()
+                    .eventDate(ZonedDateTime.now())
+                    .eventType("BUSINESS_ERROR")
+                    .body(businessError)
+                    .build());
+    log.error(errorLogMessage);
     throw new HttpStatusException(status, errorMessage);
   }
 
