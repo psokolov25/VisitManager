363abeb1c7e3acf2c15dbf929e01cc99548aa694
psokolov25 <psokolov@gmail.com>
2025-09-14 13:19:27 +0300

    test(model): покрыт класс User

---
+++ src/test/java/ru/aritmos/model/UserTest.java
@@ -0,0 +1,56 @@
+package ru.aritmos.model;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+import java.time.ZonedDateTime;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+import org.keycloak.representations.idm.GroupRepresentation;
+import ru.aritmos.keycloack.service.KeyCloackClient;
+
+class UserTest {
+    @Test
+    void constructorInitializesFieldsWithKeycloakClient() {
+        KeyCloackClient client = mock(KeyCloackClient.class);
+        when(client.isUserModuleTypeByUserName("user", "admin")).thenReturn(true);
+        List<GroupRepresentation> branches = List.of(new GroupRepresentation());
+        when(client.getAllBranchesOfUser("user")).thenReturn(branches);
+
+        User user = new User("1", "user", client);
+
+        assertTrue(user.getIsAdmin());
+        assertEquals(branches, user.getAllBranches());
+        assertEquals("user", user.getName());
+        assertSame(client, user.getKeyCloackClient());
+    }
+
+    @Test
+    void breakStatusAndDurationCalculation() {
+        User user = new User("2", "worker", null);
+        assertFalse(user.isOnBreak());
+        assertEquals(0L, user.getLastBreakDuration());
+
+        user.setLastBreakStartTime(ZonedDateTime.now());
+        assertTrue(user.isOnBreak());
+        assertTrue(user.getLastBreakDuration() >= 0);
+
+        user.setLastBreakEndTime(user.getLastBreakStartTime().plusSeconds(45));
+        assertFalse(user.isOnBreak());
+        assertEquals(45L, user.getLastBreakDuration());
+    }
+
+    @Test
+    void constructorGeneratesIdWhenNotProvided() {
+        KeyCloackClient client = mock(KeyCloackClient.class);
+        when(client.isUserModuleTypeByUserName("name", "admin")).thenReturn(false);
+        when(client.getAllBranchesOfUser("name")).thenReturn(List.of());
+
+        User user = new User("name", client);
+
+        assertNotNull(user.getId());
+        assertEquals("name", user.getName());
+        assertFalse(Boolean.TRUE.equals(user.getIsAdmin()));
+    }
+}
+
